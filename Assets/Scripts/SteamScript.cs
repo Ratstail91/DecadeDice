3904bec063096bb387c7c2af34d9441d6f8ffa66
Kayne Ruse <kayneruse@gmail.com>
2020-01-01 21:22:26 +1100

    Began working with steamworks

---
+++ Assets/Scripts/SteamScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Steamworks;
+
+class SteamScript : MonoBehaviour {
+	void Start() {
+		if (SteamManager.Initialized) {
+			string name = SteamFriends.GetPersonaName();
+			Debug.Log(name);
+		}
+	}
+}
