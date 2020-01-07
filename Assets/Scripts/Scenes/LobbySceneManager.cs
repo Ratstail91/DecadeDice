6a6da880aceccd8d0254145c9a34953d34ab2ae2
Kayne Ruse <kayneruse@gmail.com>
2020-01-08 10:56:34 +1100

    Constant crash bug in steamworks.NET

--- Assets/Scripts/Scenes/LobbySceneManager.cs
+++ Assets/Scripts/Scenes/LobbySceneManager.cs
@@ -1,10 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Steamworks;
 using TMPro;
 
-public class LobbySceneManager : MonoBehaviour, ISteamMatchmakingServerListResponse { //I need a stiff drink
+public class LobbySceneManager : MonoBehaviour { //I need a stiff drink
 	//inspector members
 	[SerializeField]
 	GameObject lobbyPanelPrefab;
@@ -17,63 +16,7 @@ public class LobbySceneManager : MonoBehaviour, ISteamMatchmakingServerListRespo
 
 	//lifetime members
 	void OnEnable() {
-		//
-
 		//kick it off
-		SearchForServers();
-	}
-
-	void OnDisable() {
-		CleanUp();
-	}
-
-	void Update() {
-		//monitor the results
-		//
-	}
-
-	//public members
-	public void SearchForServers() {
-		//Instantiate(lobbyPanelPrefab, ListContentTransform);
-
-		Debug.Log("Searching for servers");
-
-//		MatchMakingKeyValuePair_t[] filters = new MatchMakingKeyValuePair_t[1];
-//		filters[0].m_szKey = "secure";
-
-		HServerListRequest serverListRequest = SteamMatchmakingServers.RequestLANServerList(SteamUtils.GetAppID(), this); //"this"... motherf*cker
-
-		if (serverListRequest == HServerListRequest.Invalid) {
-			Debug.Log("Request is invalid");
-		}
-
-		Debug.Log(serverListRequest);
-
-		int serverCount = SteamMatchmakingServers.GetServerCount(serverListRequest);
-		Debug.Log("Servers found: " + serverCount);
-
-		for(int i = 0; i < serverCount; i++) {
-			gameserveritem_t gs = SteamMatchmakingServers.GetServerDetails(serverListRequest, i);
-			Debug.Log("Server name: " + gs.GetServerName());
-		}
-
-		SteamMatchmakingServers.ReleaseRequest(serverListRequest);
-	}
-
-	public void CleanUp() {
-		//TODO: release the request
-	}
-
-	//callbacks
-	void OnServerResponse(HServerListRequest hRequest, int iServer) {
-		Debug.Log("OnServerResponse called");
-	}
-
-	void OnServerFailedToRespond(HServerListRequest hRequest, int iServer) {
-		Debug.Log("OnServerFailedToRespond called");
-	}
-
-	void OnRefreshComplete(HServerListRequest hRequest, EMatchMakingServerResponse response) {
-		Debug.Log("OnRefreshComplete called");
+		SteamMatchmakingServerListResponse.SearchForServers();
 	}
 }
---
+++ Assets/Scripts/SteamMatchmakingServerListResponse.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Steamworks;
+
+public class SteamMatchmakingServerListResponse : ISteamMatchmakingServerListResponse {
+	//singleton members
+	public static SteamMatchmakingServerListResponse Instance {
+		get {
+			if (_self != null) {
+				return _self;
+			}
+			return _self = new SteamMatchmakingServerListResponse();
+		}
+	}
+	static SteamMatchmakingServerListResponse _self = null;
+
+	//internal members
+	static HServerListRequest serverListRequest = HServerListRequest.Invalid;
+
+	//constructor/destructor
+	SteamMatchmakingServerListResponse() : base(ServerResponded, ServerFailedToRespond, RefreshComplete) {
+		//
+	}
+
+	~SteamMatchmakingServerListResponse() {
+		ReleaseRequest();
+	}
+
+	//public members
+	public static void SearchForServers() {
+		Debug.Log("Searching for servers");
+
+		MatchMakingKeyValuePair_t[] filters = new MatchMakingKeyValuePair_t[2];
+
+		filters[0].m_szKey = "secure";
+		filters[0].m_szValue = "1";
+
+		filters[1].m_szKey = "gamedir";
+		filters[1].m_szValue = "decadedice";
+
+		serverListRequest = SteamMatchmakingServers.RequestInternetServerList(SteamUtils.GetAppID(), filters, 2, Instance);
+
+		if (serverListRequest == HServerListRequest.Invalid) {
+			Debug.Log("Request is invalid");
+		}
+	}
+
+	static void ReleaseRequest() {
+		SteamMatchmakingServers.ReleaseRequest(serverListRequest);
+		serverListRequest = HServerListRequest.Invalid;
+	}
+
+	public static void ListServers() {
+		int serverCount = SteamMatchmakingServers.GetServerCount(serverListRequest);
+		Debug.Log("Servers found: " + serverCount);
+
+		for(int i = 0; i < serverCount; i++) {
+			gameserveritem_t gs = SteamMatchmakingServers.GetServerDetails(serverListRequest, i);
+			Debug.Log("Server name: " + gs.GetServerName());
+		}
+	}
+
+	//callbacks
+	static void ServerResponded(HServerListRequest hRequest, int iServer) {
+		Debug.Log("ServerResponded called");
+
+//		ListServers(); //TODO: store the server data
+	}
+
+	static void ServerFailedToRespond(HServerListRequest hRequest, int iServer) {
+		Debug.Log("ServerFailedToRespond called");
+	}
+
+	static void RefreshComplete(HServerListRequest hRequest, EMatchMakingServerResponse response) {
+		Debug.Log("RefreshComplete called");
+
+		ReleaseRequest();
+	}
+}
